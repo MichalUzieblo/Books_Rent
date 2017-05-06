8736e789d2af4b997660165b3456928e256d3423
MichalUzieblo <michal_uzieblo@op.pl>
2017-05-06 16:59:10 +0200

    struktura

---
+++ api/src/connect.php
@@ -0,0 +1,13 @@
+<?php
+//require_once dirname(__FILE__) . "/../config/database.php";
+
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASSWORD = "coderslab";
+$DB_DBNAME = "books_rent";
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DBNAME);
+if ($conn->connect_error) {
+    die("Polaczenie z products_ex nieudane. Blad: " .
+    $conn->connect_error);
+} 
